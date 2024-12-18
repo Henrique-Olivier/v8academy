6d098594201ab93bb45fd9d54b620b03c7432832
henrique-olivier <henrique.rosa@sptech.school>
2024-12-18 13:17:04 -0300

    player finalizado

--- src/pages/player/[idAula].tsx
+++ src/pages/player/[idAula].tsx
@@ -2,15 +2,27 @@ import { supabase } from '@/service/supabase';
 import { get } from 'http';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-
+import { ContainerContent, Iframe, VideoFrame, Header, ButtonDiv } from './style'
+import Layout from '@/components/Layout';
+import { Button } from 'react-bootstrap';
 interface Aula {
     id: number;
     titulo: string;
     url: string;
-    fkModulo: number;   
+    modulo: Modulo;
 }
 
+interface Modulo {
+    id: number;
+    titulo: string;
+    curso: Curso;
+}
 
+interface Curso {
+    id: number;
+    titulo: string;
+    descricao: string;
+}
 
 export default function AulaPage() {
     const router = useRouter();
@@ -23,35 +35,61 @@ export default function AulaPage() {
 
 
     async function getAula() {
-        const {data, error} = await supabase
-        .from('aula')
-        .select("*")
-        .eq('idAula', idAula)
+        const { data, error } = await supabase
+            .from('aula')
+            .select(`
+                *, 
+                modulo:fkModulo (
+                    id, 
+                    titulo, 
+                    curso:fkCurso (
+                        id,
+                        titulo,
+                        descricao
+                    )
+                )
+            `)
+            .eq('idAula', idAula)
+            .single();
 
         if (error) {
-            console.log(error)
-            return
+            console.log(error);
+            return;
         }
 
         if (data) {
-            setAula(data[0])
+            console.log(data)
+            setAula(data);
         }
-        console.log(data)
+
+        console.log(data);
     }
 
 
 
+
     return (
-        <div>
-            <h1>Detalhes da Aula</h1>
-            <p>{aula && aula.titulo}</p>
-            <iframe
-                width="560"
-                height="315"
-                src={aula && aula.url}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
-        </div>
+        <Layout>
+            <ContainerContent>
+                <Header>
+                    <p>{aula && `${aula.modulo.curso.titulo} > ${aula.modulo.titulo}`}</p>
+                    <h1>{aula && aula.titulo}</h1>
+                </Header>
+                <VideoFrame>
+                    <Iframe
+                        width="560"
+                        height="315"
+                        src={aula && aula.url}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></Iframe>
+                </VideoFrame>
+                <ButtonDiv>
+                    <Button className='px-5 py-2' variant="primary">
+                        Próxima aula
+                    </Button>
+                </ButtonDiv>
+            </ContainerContent>
+        </Layout>
     );
 }
