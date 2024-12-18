7bbb315a2778766de206c5408f6e0a811250e2f4
henrique-olivier <henrique.rosa@sptech.school>
2024-12-18 10:58:33 -0300

    organizando arquivos

---
+++ src/components/Layout/style.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;    
+`
+export const Content = styled.div`
+width: 78%;
+height: 100vh;
+`
+
+export const SidebarContainer = styled.div`
+    width: 22%;
+    height: 100vh;
+    background-color: #F8F9FA;
+    display: flex;
+    flex-direction: column; 
+    justify-content: start;
+    align-items: center;
+    gap: 20px;
+`
+
+export const List = styled.ul`
+    width: 100%;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    margin-top: 50px;
+    padding: 0;
+    `
+
+
+export const ListItem = styled.li`
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 600;
+    font-size: 18px;
+    margin-left: 50px;
+`
+
+export const DivLine = styled.div<{$hasTopBorder?: boolean}>`
+    width: 100%;
+    height: 50px;
+    border-top: ${({ $hasTopBorder }) => $hasTopBorder ? '1px solid #D9D9D9' : 'none'};  
+    border-bottom: 1px solid #D9D9D9;
+    display: flex;  
+    align-items: center;
+    cursor: pointer;      
+`
