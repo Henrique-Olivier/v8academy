84379e435cb28327012d44a904af91c3de2ca91a
henrique-olivier <henrique.rosa@sptech.school>
2024-12-18 14:49:18 -0300

    corrigindo conflito

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true, // Habilita o suporte ao styled-components no SWC
+  },
 };
 
 export default nextConfig;
