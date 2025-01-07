3dcf7d115b313959a3e2eefec398ea6e34303c51
henrique-olivier <henrique.rosa@sptech.school>
2025-01-07 20:01:38 -0300

    user authentication

--- src/pages/courses/[idTrail].tsx
+++ src/pages/courses/[idTrail].tsx
@@ -43,6 +43,9 @@ margin: 0;
 const InputBox = styled.div`
 width: 65%;
 padding: 10px 80px; 
+display: flex;
+flex direction: row;
+
 `
 
 const GridCourses = styled.div`
@@ -55,6 +58,9 @@ gap: 40px;
 const DivBtn = styled.div`
 width: 60%;
 padding: 10px 10px;
+`
+const SearchBar = styled.div `
+width: 100%;
 `
 
 interface TrilhaCurso {
@@ -89,8 +95,12 @@ export default function CoursePage() {
   const [courses, setCourses] = useState<TrilhaCurso[]>([]);
   const [trail, setTrail] = useState<Trilha>();
   const [allCourses, setAllCourses] = useState<Curso[]>([]);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+
   useEffect(() => {
 
+    authenticate()
     if (idTrail == "0") {
       getAllCourses()
     } else {
@@ -100,7 +110,37 @@ export default function CoursePage() {
 
   }, [idTrail])
 
+  async function authenticate() {
+    const userObject = localStorage.getItem("sb-bfogzwmikqkepnhxrjyt-auth-token");
+    if (userObject) {
+      const parsedUserObject = JSON.parse(userObject)
+
+      const userId = parsedUserObject.user.id
+
+      const { data, error } = await supabase
+      .from('usuario')
+      .select('*')
+      .eq('id', userId)
+
+      if(error) {
+        return
+      }
+
+      if(data[0].isAdmin) {
+        setIsAdmin(true);
+
+        console.log('admin')
+      } else {
+        setIsAdmin(false)
+      }
+    } else {
+      router.push('/login')
+    }
+  }
+
   async function getAllCourses() {
+    'use client'
+
     try {
       const { data, error } = await supabase
         .from("curso")
@@ -181,21 +221,20 @@ export default function CoursePage() {
   function showCourses(courses: TrilhaCurso[]) {
     if (courses) {
       return courses.map((course) => (
-        <>
-          <Card style={{ width: '18rem' }} key={course.curso.id}>
-            <Card.Body>
-              <Card.Title>{course.curso.titulo}</Card.Title>
-              <Card.Text>
-                {course.curso.descricao}
-              </Card.Text>
-            </Card.Body>
-            <DivBtn>
-              <Button variant="primary" onClick={() => {
-                router.push(`/lessons/${course.curso.id}`)
-              }}>Ver curso</Button>
-            </DivBtn>
-          </Card>
-        </>
+
+        <Card style={{ width: '18rem' }} key={course.curso.id}>
+          <Card.Body>
+            <Card.Title>{course.curso.titulo}</Card.Title>
+            <Card.Text>
+              {course.curso.descricao}
+            </Card.Text>
+          </Card.Body>
+          <DivBtn>
+            <Button variant="primary" onClick={() => {
+              router.push(`/lessons/${course.curso.id}`)
+            }}>Ver curso</Button>
+          </DivBtn>
+        </Card>
       ));
     }
     return null;
@@ -231,12 +270,14 @@ export default function CoursePage() {
           <h1>Todos os cursos:</h1>
         </Header>
         <InputBox>
-          <div className="search">
+          <SearchBar>
             <div className="input-group flex-nowrap">
               <input type="text" className="form-control" placeholder="Search..." aria-label="Username" aria-describedby="addon-wrapping" />
               <span className="input-group-text" id="addon-wrapping"><Image src={searchIcon} alt="Icon de procura" /></span>
             </div>
-          </div>
+          </SearchBar>
+
+          {isAdmin ? <Button variant="primary">Adicionar Curso</Button> : ''}
         </InputBox>
 
         <GridCourses>
