81267f5b6a339f2f127262448ef60b0379ea1b37
henrique-olivier <henrique.rosa@sptech.school>
2025-01-21 08:46:05 -0300

    corrigindo next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true, // Habilita o suporte ao styled-components no SWC
   },
+  async redirects() {
+    return [
+      {
+        source: '/', // Rota inicial
+        destination: '/login', // Rota de destino
+        permanent: false, // Use true para redirecionamento permanente
+      },
+    ];
+  },
 };
 
 export default nextConfig;
